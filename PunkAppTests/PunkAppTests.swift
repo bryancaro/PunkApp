d3df2a161df7bd6d0a1a1329a5ec6cea085a1760
Bryan Caro <bryancaro@icloud.com>
2021-05-27 00:29:43 +0200

    Last commited

--- PunkAppTests/PunkAppTests.swift
+++ PunkAppTests/PunkAppTests.swift
@@ -9,13 +9,47 @@ import XCTest
 @testable import PunkApp
 
 class PunkAppTests: XCTestCase {
-
+    var sut: URLSession!
+    let networkMonitor = BeerService?.self
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      try super.setUpWithError()
+      sut = URLSession(configuration: .default)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+      sut = nil
+      try super.tearDownWithError()
+    }
+    
+    // Asynchronous test: success fast, failure slow
+    func testValidApiCallGetsHTTPStatusCode200() throws {
+      
+
+      // given
+      let urlString = "https://api.punkapi.com/v2/beers?page=1&per_page=20"
+      let url = URL(string: urlString)!
+      // 1
+      let promise = expectation(description: "Status code: 200")
+
+      // when
+      let dataTask = sut.dataTask(with: url) { _, response, error in
+        // then
+        if let error = error {
+          XCTFail("Error: \(error.localizedDescription)")
+          return
+        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+          if statusCode == 200 {
+            // 2
+            promise.fulfill()
+          } else {
+            XCTFail("Status code: \(statusCode)")
+          }
+        }
+      }
+      dataTask.resume()
+      // 3
+      wait(for: [promise], timeout: 5)
     }
 
     func testExample() throws {
@@ -31,3 +65,19 @@ class PunkAppTests: XCTestCase {
     }
 
 }
+
+/*
+ @testable import PunkApp
+
+ class PunkAppUITests: XCTestCase {
+     var sut: BeerService!
+     override func setUpWithError() throws {
+         try super.setUpWithError()
+         sut = BeerService()
+     }
+
+     override func tearDownWithError() throws {
+         sut = nil
+         try super.tearDownWithError()
+     }
+ */
--- PunkAppUITests/PunkAppUITests.swift
+++ PunkAppUITests/PunkAppUITests.swift
@@ -6,16 +6,14 @@
 //
 
 import XCTest
-
 class PunkAppUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDownWithError() throws {
